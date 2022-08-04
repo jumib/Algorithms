e3979ca1d5b3c8bb7bdd0e0ef8be593b34a7df5d
jumib <baekjumi@naver.com>
2022-08-04 19:53:21 +0900

    [6] 가장 짧은 문자열

--- src/js/distance.js
+++ src/js/distance.js
@@ -1,14 +1,31 @@
 function solution(s, t){
-    let a=0;
-    let l=[];
-    if(s.indexOf(t)>-1){
-        
+    //앞에서 뒤 비교하면서 값  넣어준다.
+    //뒤에서 앞으로 값  비교
+    let arr = [];
+    let cnt = 0;
+
+    for(i=0; i<s.length-1; i++){
+        if(s[i] === t){
+            cnt = 0;
+            arr.push(cnt);
+        } else {
+            cnt++;
+            arr.push(cnt);
+        }
     }
-   
-    for(i=0; i<s.length;i++){
-        
+    cnt = 0;
+    for(i=s.length-1;i>=0;i--){
+        if(s[i] === t){
+            cnt=0;
+            arr[i]=cnt;
+        } else {
+            cnt++;
+            arr[i] = Math.min(arr[i], cnt);
+        }
     }
+    return arr
+
 }
 
 let str="teachermode";
-console.log(solution(str, 'e'));
+console.log(solution(str,'e'));
